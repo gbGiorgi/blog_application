0f04aadcff2384144c673a7a0402db19fbee1622
= <=>
2022-06-30 15:57:51 +0400

    admin functionalities

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -18,6 +18,7 @@ class CommentsController < ApplicationController
   def destroy
     @comment = @post.comments.find(params[:id])
     @comment.destroy
+    flash[:alert] = 'The comment has been deleted'
     redirect_to post_path(@post)
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,24 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-User.create(email: "bobokha@gmail.com", name: "Seed name", password: "giorguna123", password_confirmation: "giorguna123")
+User.create(email: "bobokha@gmail.com",
+            name: "სიდი{admin}",
+            password: "giorguna123",
+            password_confirmation: "giorguna123",
+            role: User.roles[:admin])
+User.create(email: "bobokhidze@gmail.com",
+            name: "სიდი{user}",
+            password: "giorguna123",
+            password_confirmation: "giorguna123")
 
-10.times do |x|
-  Post.create(title: "Title #{x}", body: "it's post number #{x}", user_id: User.first.id)
+
+5.times do |x|
+  post = Post.create(title: "Post number #{x}",
+                     body: "This body is for post number #{x}",
+                     user_id: User.first.id)
+  5.times do |y|
+    Comment.create(body: "Comment number #{y}",
+                   user_id: User.second.id,
+                   post_id: post.id)
+  end
 end
