7e219829a242f7026e39b11095ff41c124e1757f
= <=>
2022-07-19 16:55:42 +0400

    fixed another bug

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,7 +1,7 @@
 
 Rails.configuration.stripe = {
-    :publishable_key => Rails.application.credentials.production[:stripe_public_key],
+    :public_key => Rails.application.credentials.production[:stripe_public_key],
   
-    :secret_key      => Rails.application.credentials.production[:stripe_private_key]
+    :private_key => Rails.application.credentials.production[:stripe_private_key]
    }
    Stripe.api_key =  Rails.application.credentials.production[:stripe_private_key]
