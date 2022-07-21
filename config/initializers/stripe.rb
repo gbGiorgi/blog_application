8d998566de5bd1438c3cf0b132182d7f806b668f
= <=>
2022-07-21 09:24:59 +0400

    stripe

--- config/initializers/stripe.rb
+++
@@ -1,7 +0,0 @@
-
-Rails.configuration.stripe = {
-    :public_key => Rails.application.credentials.production[:stripe_public_key],
-  
-    :private_key => Rails.application.credentials.production[:stripe_private_key]
-   }
-   Stripe.api_key =  Rails.application.credentials.production[:stripe_private_key]
