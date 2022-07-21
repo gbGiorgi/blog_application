8482c5077903d9fccea8a2b7f5c016b3fdba86c7
= <=>
2022-07-21 12:11:49 +0400

    master key

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,10 +1,8 @@
 # frozen_string_literal: true
 
 require 'active_support/core_ext/integer/time'
-require 'stripe'
 
 Rails.application.configure do
-  Stripe.api_key = Rails.application.credentials.secret_access_key
   config.after_initialize do
     Bullet.enable        = true
     Bullet.alert         = true
