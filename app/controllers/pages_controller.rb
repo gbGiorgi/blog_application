7dd730ba60c4cd265f0502f5b45cdb083199ccdc
= <=>
2022-07-21 10:02:22 +0400

    stripe error

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,10 +2,10 @@
 
 class PagesController < ApplicationController
   def home
-    return unless current_user
-    return if current_user.payment_processor.nil?
-
-    @portal_session = current_user.payment_processor.billing_portal
+    # return unless current_user
+    # return if current_user.payment_processor.nil?
+    #
+    # @portal_session = current_user.payment_processor.billing_portal
   end
 
   def about
