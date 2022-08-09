596c45a54dfc930f64a0842b42fd3e1d9dbeeb67
= <=>
2022-08-09 09:30:28 +0400

    modified views

---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe UsersController, type: :controller do
+
+  let(:id) { User.last.id }
+
+  describe "before_action" do
+    it { should use_before_action(:set_user)}
+  end
+
+  describe "set_user" do
+
+  end
+end
